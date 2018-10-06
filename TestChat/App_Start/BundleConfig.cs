99fca7e6ceabef7f5952e3e139e898ac9d28119b
Efremov Aleksey <efremov_a@danfoss.com>
2018-10-06 13:03:33 +0300

    Add simple hub

--- TestChat/App_Start/BundleConfig.cs
+++ TestChat/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace TestChat
         public static void RegisterBundles(BundleCollection bundles)
         {
              bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.3.1.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery.signalR-3.3.0.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ TestChat/Hubs/ChutHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestChat.Hubs
+{
+    public class ChutHub:Hub
+    {
+        public void Send()
+        {
+            Clients.All.addMessage();
+        }
+    }
+}
